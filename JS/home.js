fca213b161b5a03621297f73f5cb105078f0e77a
mdmonjumon <mdmonjumon786@gmail.com>
2024-12-02 19:14:50 +0600

    write javascript for cashout

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,9 @@
+/*Script for logout button */
 document.getElementById('logout-btn').addEventListener('click', function () {
     window.location.href = './index.html';
 })
 
+/*Script for add money and Cash Out button */
 document.getElementById('add-money-btn').addEventListener('click', function () {
     document.getElementById('add-money-container').classList.remove('hidden');
     document.getElementById('cash-out-container').classList.add('hidden');
@@ -11,6 +13,8 @@ document.getElementById('cash-out-btn').addEventListener('click', function () {
     document.getElementById('cash-out-container').classList.remove('hidden');
     document.getElementById('add-money-container').classList.add('hidden');
 })
+
+/*Script for add money */
 
 document.getElementById('add-money').addEventListener('click', function (event) {
     event.preventDefault();
@@ -29,5 +33,29 @@ document.getElementById('add-money').addEventListener('click', function (event) 
     }
     else {
         alert('Type your amount and pin code');
+    };
+});
+
+
+/*Script for Cash Out */
+
+document.getElementById('cash-out').addEventListener('click', function(event){
+    event.preventDefault();
+    const cashOutAmount = document.getElementById('cash-out-amount').value;
+    const cashOutAmountNumber = parseFloat(cashOutAmount);
+    const balanceBeforeCashOut = document.getElementById('balance').innerText;
+    const balanceBeforeCashOutNumber = parseFloat(balanceBeforeCashOut);
+    const cashOutPin = document.getElementById('cash-out-pin').value;
+    if(cashOutPin === '4444'){
+        const balanceAfterCashOut = balanceBeforeCashOutNumber - cashOutAmountNumber;
+        document.getElementById('balance').innerText = balanceAfterCashOut;
+        alert('Cash Out successful');
+        document.getElementById('cash-out-amount').value = '';
+        document.getElementById('cash-out-pin').value = '';
+
     }
-})
+    else{
+        alert('Type your amount and pin code');
+    }
+
+});
